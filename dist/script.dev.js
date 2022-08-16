201d73cc5ec7138dfe1cc939adcc19c6f28fabfe
isonguyom <isonguyom@gmail.com>
2022-08-16 15:30:13 +0100

    setup jquery

---
+++ dist/script.dev.js
@@ -0,0 +1,6 @@
+"use strict";
+
+$(document).ready(function () {
+  // jQuery methods go here...
+  $(".title").text("Hello! this is a password strength checker");
+});
---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+
+    // jQuery methods go here...
+    
+    $(".title").text("Hello! this is a password strength checker")
+
+});
